664b771ad4dc40c6e3880d21ceac36a9fa14f3bd
love1119 <goldenstar007.dev@gmail.com>
2023-10-30 15:51:24 -0400

    minor change

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -7,6 +7,7 @@ import {
   CardSubtitle,
   CardText,
   Col,
+  NavLink,
 } from "reactstrap";
 
 import { Fade } from "react-reveal";
@@ -20,7 +21,7 @@ const ExperienceCard = ({ data }) => {
           className="shadow-lg--hover mb-3 shadow border-0 text-center rounded"
         >
           <CardBody className="">
-            <a href={data.link} target="_blank">
+            <a href={data.link} target="_blank" rel="noreferrer">
               <img
                 src={data.companylogo}
                 style={{
